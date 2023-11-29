7eb9b1198bc8a8aabaa864f43eb87544edbad147
JonathanMPeres <emailmarceloperes@gmail.com>
2023-11-29 14:02:48 -0300

    first commit

---
+++ Apple-Frameworks-App/Apple_Frameworks_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Frameworks_AppApp.swift
+//  Apple-Frameworks-App
+//
+//  Created by Jonathan Marcelo Peres on 29/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Frameworks_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple-Frameworks-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apple-Frameworks-App
+//
+//  Created by Jonathan Marcelo Peres on 29/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
