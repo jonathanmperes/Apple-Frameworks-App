010fae1af042f659423cc863f10fdeb23dcc1de9
JonathanMPeres <emailmarceloperes@gmail.com>
2023-11-30 11:30:40 -0300

    struct Framework set

---
+++ Apple-Frameworks-App/Framework.swift
@@ -0,0 +1,15 @@
+//
+//  Framework.swift
+//  Apple-Frameworks-App
+//
+//  Created by Jonathan Marcelo Peres on 30/11/23.
+//
+
+import Foundation
+
+struct Framework {
+    let name: String
+    let imageName: String
+    let urlString: String
+    let description: String
+}
