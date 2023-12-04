6b0c1ed938298d390419665fd82b2d1e87e3220f
JonathanMPeres <emailmarceloperes@gmail.com>
2023-12-04 10:23:54 -0300

    FrameworkGridView created

---
+++ Apple-Frameworks-App/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks-App
+//
+//  Created by Jonathan Marcelo Peres on 04/12/23.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
