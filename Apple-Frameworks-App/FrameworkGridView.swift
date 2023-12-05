cc1f5f8e119a0e638d20c6fe630fb855a648390c
JonathanMPeres <emailmarceloperes@gmail.com>
2023-12-05 10:49:48 -0300

    Image added to VStack in FrameworkGridView

--- Apple-Frameworks-App/FrameworkGridView.swift
+++ Apple-Frameworks-App/FrameworkGridView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct FrameworkGridView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("app-clip")
+                .resizable()
+                .frame(width: 90, height: 90)
+        }
     }
 }
 
