0f9336bfe1d2d2aa62eaea7e85857fda340b45e0
JonathanMPeres <emailmarceloperes@gmail.com>
2023-12-15 15:46:11 -0300

    FrameworkTitleView refactored

--- Apple-Frameworks-App/FrameworkGridView.swift
+++ Apple-Frameworks-App/FrameworkGridView.swift
@@ -31,15 +31,14 @@ struct FrameworkGridView_Previews: PreviewProvider {
 
 struct FrameworkTitleView: View {
     
-    let name: String
-    let imageName: String
+    let framework: Framework
     
     var body: some View {
         VStack {
-            Image(imageName)
+            Image(framework.imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
-            Text(name)
+            Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
