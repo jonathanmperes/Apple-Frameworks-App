b184b46ee5a43c2892147d4563fa3a9f36cbeb55
JonathanMPeres <emailmarceloperes@gmail.com>
2023-12-18 11:01:13 -0300

    FrameworkDetailView create

---
+++ Apple-Frameworks-App/FrameworkDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkDetailView.swift
+//  Apple-Frameworks-App
+//
+//  Created by Jonathan Marcelo Peres on 18/12/23.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView()
+    }
+}
