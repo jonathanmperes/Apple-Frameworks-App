0921a2939120d59e7b8cd2a277708eec9e236796
JonathanMPeres <emailmarceloperes@gmail.com>
2023-12-21 13:31:53 -0300

    X button added to FrameworkDetailView

--- Apple-Frameworks-App/FrameworkDetailView.swift
+++ Apple-Frameworks-App/FrameworkDetailView.swift
@@ -13,6 +13,20 @@ struct FrameworkDetailView: View {
     
     var body: some View {
         VStack {
+            HStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 40, height: 40)
+                }
+            }
+            .padding()
+            
             Spacer()
             
             FrameworkTitleView(framework: framework)
