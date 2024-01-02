887fea8950b4d21a832c16fa91e6043a2af1cbfd
JonathanMPeres <emailmarceloperes@gmail.com>
2024-01-02 10:33:35 -0300

    isShowingDetailView set in FrameworkGridViewModel

--- Apple-Frameworks-App/FrameworkGridViewModel.swift
+++ Apple-Frameworks-App/FrameworkGridViewModel.swift
@@ -9,5 +9,11 @@ import SwiftUI
 
 final class FrameworkGridViewModel: ObservableObject {
     
-    var selectedFramework: Framework?
+    var selectedFramework: Framework? {
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+    
+    @Published var isShowingDetailView = false
 }
