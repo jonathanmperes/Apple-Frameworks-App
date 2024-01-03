dd43b2a94b8d1ca94cc396d9a5bf482f94177edd
JonathanMPeres <emailmarceloperes@gmail.com>
2024-01-03 09:28:20 -0300

    struct SafariView created

---
+++ Apple-Frameworks-App/SafariView.swift
@@ -0,0 +1,20 @@
+//
+//  SafariView.swift
+//  Apple-Frameworks-App
+//
+//  Created by Jonathan Marcelo Peres on 03/01/24.
+//
+
+import SwiftUI
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        <#code#>
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        <#code#>
+    }
+    
+}
