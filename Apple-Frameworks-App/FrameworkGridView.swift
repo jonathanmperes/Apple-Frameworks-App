603ceca14040c9fdfbab987c0e5add7c0dfdc0a2
JonathanMPeres <emailmarceloperes@gmail.com>
2024-01-03 09:18:18 -0300

    sheet in FrameworkGridView updated

--- Apple-Frameworks-App/FrameworkGridView.swift
+++ Apple-Frameworks-App/FrameworkGridView.swift
@@ -30,7 +30,7 @@ struct FrameworkGridView: View {
             }
             .navigationTitle("Apple Frameworks")
             .sheet(isPresented: $viewModel.isShowingDetailView) {
-                FrameworkDetailView(framework: viewModel.selectedFramework ?? MockData.sampleFramework)
+                FrameworkDetailView(framework: viewModel.selectedFramework ?? MockData.sampleFramework, isShowingDetailView: $viewModel.isShowingDetailView)
             }
         }
     }
