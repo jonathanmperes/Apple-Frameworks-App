b519ded72d4246896925f3830395d523b06a644b
JonathanMPeres <emailmarceloperes@gmail.com>
2024-01-04 08:13:51 -0300

    FrameworkTitleView moved to its own file

--- Apple-Frameworks-App/FrameworkGridView.swift
+++ Apple-Frameworks-App/FrameworkGridView.swift
@@ -37,22 +37,3 @@ struct FrameworkGridView_Previews: PreviewProvider {
             .preferredColorScheme(.dark)
     }
 }
-
-struct FrameworkTitleView: View {
-    
-    let framework: Framework
-    
-    var body: some View {
-        VStack {
-            Image(framework.imageName)
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text(framework.name)
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.6)
-        }
-        .padding()
-    }
-}
