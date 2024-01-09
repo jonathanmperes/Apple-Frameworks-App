e5af06ff4caadad881be6d2da675c5f626e4c0e1
JonathanMPeres <emailmarceloperes@gmail.com>
2024-01-09 16:47:25 -0300

    FrameworkTitleView lyout updated

--- Apple-Frameworks-App/Views/View/FrameworkTitleView.swift
+++ Apple-Frameworks-App/Views/View/FrameworkTitleView.swift
@@ -15,14 +15,14 @@ struct FrameworkTitleView: View {
         HStack {
             Image(framework.imageName)
                 .resizable()
-                .frame(width: 90, height: 90)
+                .frame(width: 70, height: 70)
             Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
+                .padding()
         }
-        .padding()
     }
 }
 
